d19a63d8c2b07121ef2c28c1d208ab24a58d3bf2
Eric Osburn <bjonesen@gmail.com>
2013-10-10 23:44:56 -0500

    Add new version of robot code
     - Threaded with pthreads
     - Implements BuckBeak requirements
     - Untested as of yet - Not even a compilation test

---
+++ Robit.cpp
@@ -0,0 +1,141 @@
+/*************************************************************************************
+* Robit.cpp
+*
+* Defines the main robot class, as well as the thread functions
+*************************************************************************************/
+
+/*************************************************************************************
+Includes
+*************************************************************************************/
+#include "Robit.h"
+
+#include "pthread.h"
+#include "semaphore.h"
+#include "time.h"
+#include "unistd.h"
+#include "WPILib.h"
+
+/*************************************************************************************
+Global variables
+*************************************************************************************/
+sem_t shooter_semaphore;
+
+/*************************************************************************************
+Robot class constructor
+*************************************************************************************/
+myRobit::myRobit(){
+	// Set up the shooter's semaphore
+	sem_init( &shooter_semaphore, 0, 0 );
+}
+
+/*************************************************************************************
+Robot class destructor
+*************************************************************************************/
+myRobit::~myRobit(){}
+
+/*************************************************************************************
+RobotInit
+ - Runs after all OS setup
+*************************************************************************************/
+void myRobit::RobotInit(){
+	// Set up the control objects
+	compressor =		new Compressor( PRESSURE_SWITCH, COMPRESSOR_PORT );
+	drivetrain =		new RobotDrive( FRONT_LEFT_MOTOR, BACK_LEFT_MOTOR, FRONT_RIGHT_MOTOR, BACK_RIGHT_MOTOR );
+	joystick =			new Joystick( JOYSTICK_PORT );
+	shooter_motor =		new Talon( TALON_PORT );
+	shooter_piston =	new DoubleSolenoid( SHOOTER_PISTON_FORE, SHOOTER_PISTON_BACK );
+
+	// Set all starting values for objects
+	compressor->Start();
+	shooter_piston->Set( kReverse );
+
+	// Set up thread argument structs
+	struct drive_arg driveArgStruct = { .drivetrain = drivetrain, .joystick = joystick };
+	struct input_arg inputArgStruct = { .joystick = joystick };
+	struct shooter_arg shooterArgStruct = { .shooter_piston = shooter_piston, .shooter_motor = shooter_motor };
+
+	// Start all the threads
+	pthread_create( &driveThread, NULL, driveFunc, (void *) &driveArgStruct );
+	pthread_create( &inputThread, NULL, inputFunc, (void *) &inputArgStruct );
+	pthread_create( &shooterThread, NULL, shooterFunc, (void *) &shooterArgStruct );
+}
+
+/*************************************************************************************
+AutonomousInit
+ - Runs once at start of autonomous period
+*************************************************************************************/
+void myRobit::AutonomousInit(){
+	// Fire four times
+	sem_post_multiple( &shooter_semaphore, 4 );
+}
+
+/*************************************************************************************
+DriveFunc
+ - Drives the robot asynchronously
+*************************************************************************************/
+void * myRobit::driveFunc( void * arg ){
+	struct drive_arg * driveArg = (struct drive_arg *) arg;
+											/* Cast argument						*/
+
+	while ( 1 ){
+		// Do dat drive thang
+		driveArg->drivetrain->MecanumDrive_Cartesian(
+			driveArg->joystick->GetRawAxis( AXIS_LEFT_X ),
+			driveArg->joystick->GetRawAxis( AXIS_LEFT_Y ), 
+			driveArg->joystick->GetRawAxis( AXIS_RIGHT_X ) );
+	}
+}
+
+/*************************************************************************************
+InputFunc
+ - If the shoot button is pushed, post to the shooters semaphore
+*************************************************************************************/
+void * myRobit::inputFunc( void * arg ){
+	struct input_arg * inputArg = (struct input_arg *) arg;
+											/* Cast argument						*/
+	struct timespec waitstruct = { .tv_spec = 0, .tv_nsec = 200000000 };
+											/* Set up wait structure				*/
+
+	while ( 1 ){
+		// Post the semaphore if button push, debounce for 200 ms
+		if( inputArg->joystick->GetRawButton( BTN_A ) ){
+			sem_post( &shooter_semaphore );
+			nanosleep( &waitstruct, NULL );
+		}
+	}
+}
+
+/*************************************************************************************
+shooterFunc
+ - Waits on the semaphore, then fires
+*************************************************************************************/
+void * myRobit::shooterFunc( void * arg ){
+	struct shooter_arg * shooterArg = (struct shooter_arg *) arg;
+											/* Cast argument						*/
+	struct timespec waitstruct = { .tv_spec = 0, .tv_nsec = 500000000 };
+											/* Set up wait structure				*/
+	int val;								/* Used for checking semaphore value	*/
+	
+	while( 1 ){
+		// Wait for the semaphore
+		sem_wait( &shooter_semaphore );
+
+		// Start the shooter motor
+		shooterArg->shooter_motor->Set( 1.0 );
+		sleep( 1 );
+
+		// Pulse the firing piston
+		shooterArg->shooter_piston->Set( kForward );
+		nanosleep( &waitstruct, NULL );
+		shooterArg->shooter_piston->Set( kReverse );
+
+		// Only stop the motor if we arent firing again
+		sem_getvalue( &shooter_semaphore, &val );
+		if( !val ){
+			shooterArg->shooter_motor->Set( 0.0 );
+		}
+	}
+}
+
+// Cruddy FIRST start macro function
+START_ROBOT_CLASS( myRobit );
---
+++ Robit.h
@@ -0,0 +1,86 @@
+/*************************************************************************************
+* Robit.h
+*
+* Declares the main robot class, and the argument structs for each thread
+*************************************************************************************/
+
+/*************************************************************************************
+Includes
+*************************************************************************************/
+#include "WPILib.h"
+
+#include "pthread.h"
+#include "semaphore.h"
+
+/*************************************************************************************
+Defines
+*************************************************************************************/
+#define FRONT_RIGHT_MOTOR	1
+#define FRONT_LEFT_MOTOR	2
+#define BACK_RIGHT_MOTOR	3
+#define BACK_LEFT_MOTOR		4
+
+#define COMPRESSOR_PORT		6
+#define JOYSTICK_PORT		1
+#define PRESSURE_SWITCH		1
+#define SHOOTER_PISTON_FORE	1
+#define SHOOTER_PISTON_BACK	2
+#define TALON_PORT			5
+
+#define AXIS_LEFT_X			1
+#define AXIS_LEFT_Y			2
+#define AXIS_RIGHT_X		3
+#define AXIS_RIGHT_Y		4
+
+#define BTN_A				1
+
+/*************************************************************************************
+Main robot class declaration
+*************************************************************************************/
+class myRobit : public IterativeRobot {
+public:
+	myRobit();
+	~myRobit();
+
+	void RobotInit();
+
+	void AutonomousInit();
+
+	void * driveFunc( void * );
+	void * inputFunc( void * );
+	void * shooterFunc( void * );
+
+private:
+	pthread_t			driveThread;
+	pthread_t			inputThread;
+	pthread_t			shootThread;
+
+	Compressor *		compressor;			/* Compressor for pneumatics			*/
+	RobotDrive *		drivetrain;			/* Drivetrain - Mecanum					*/
+	Joystick *			joystick;			/* Joystick for input					*/
+	Talon *				shooter_motor;		/* CIM for shooting						*/
+	DoubleSolenoid *	shooter_piston;		/* Piston for firing discs				*/
+};
+
+/*************************************************************************************
+Drive thread argument
+*************************************************************************************/
+struct drive_arg {
+	RobotDrive *		drivetrain;
+	Joystick *			joystick;
+};
+
+/*************************************************************************************
+Input thread argument
+*************************************************************************************/
+struct input_arg {
+	Joystick *			joystick;
+};
+
+/*************************************************************************************
+Shooter thread argument
+*************************************************************************************/
+struct shooter_arg {
+	DoubleSolenoid *	shooter_piston;
+	Talon *				shooter_motor;
+};
