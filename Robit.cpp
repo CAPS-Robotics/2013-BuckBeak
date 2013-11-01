7926d934aa9a102030599298487515d2024a8036
Eric Osburn <bjonesen@gmail.com>
2013-11-01 13:16:37 -0500

    Update Robit.cpp

--- Robit.cpp
+++ Robit.cpp
@@ -44,6 +44,18 @@ myRobit::myRobit(){
 *************************************************************************************/
 myRobit::~myRobit(){}
 
+/*************************************************************************************
+Unused class functions
+*************************************************************************************/
+void myRobit::DisabledInit(){};
+void myRobit::TeleopInit(){};
+void myRobit::TestInit(){};
+
+void myRobit::DisabledPeriodic(){};
+void myRobit::AutonomousPeriodic(){};
+void myRobit::TeleopPeriodic(){};
+void myRobit::TestPeriodic(){};
+
 /*********************************************************************************//**
 * Robot init function
 *
@@ -121,24 +133,50 @@ void * inputFunc( void * arg ){
 *************************************************************************************/
 void * shooterFunc( void * arg ){
     int val;                                /* Used for checking semaphore value    */
+    int sem_retval;                         /* Used for checking semaphore wait     */
 
     while( 1 ){
-        // Wait for the semaphore
-        sem_wait( shooter_semaphore );
+        // Try to wait for the semaphore
+        sem_retval = sem_trywait( shooter_semaphore );
 
-        // Start the shooter motor
-        shooter_motor->Set( 1.0 );
-        Wait( 2.5 );
-
-        // Pulse the firing piston
-        shooter_piston->Set( shooter_piston->kForward );
-        Wait( .75 );
-        shooter_piston->Set( shooter_piston->kReverse );
-
-        // Only stop the motor if we aren't firing again
-        sem_getvalue( shooter_semaphore, &val );
-        if( !val ){
-            shooter_motor->Set( 0.0 );
+        // If we got the semaphore lock
+        if( sem_retval == 0 ){
+            // Start the shooter motor
+            shooter_motor->Set( 1.0 );
+            Wait( 2.75 );
+    
+            // Pulse the firing piston
+            shooter_piston->Set( shooter_piston->kForward );
+            Wait( .75 );
+            shooter_piston->Set( shooter_piston->kReverse );
+    
+            // Only stop the motor if we aren't firing again
+            sem_getvalue( shooter_semaphore, &val );
+            if( !val ){
+                shooter_motor->Set( 0.0 );
+            }
+            
+        } else {
+            // If manual mode button is held
+            if( joystick->GetRawButton( BTN_LBM ) ){
+                // If the motor trigger held
+                if( joystick->GetRawButton( BTN_LTG ) ){
+                    // Turn motor on
+                    shooter_motor->Set( 1.0 );
+                } else {
+                    // Keep motor off
+                    shooter_motor->Set( 0.0 );
+                }
+                
+                // If the piston trigger held
+                if(joystick->GetRawButton( BTN_RTG ) ){
+                    // Extend the piston
+                    shooter_piston->Set( shooter_piston->kForward );
+                } else {
+                    // Retract the piston
+                    shooter_piston->Set( shooter_piston->kReverse );
+                }
+            }
         }
     }
 }
