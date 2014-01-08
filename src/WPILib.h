1694bda1c1a060e071500cfafcedb86720147700
Jacob Hegna <jacobhegna@gmail.com>
2014-01-08 09:08:19 -0600

    Standardized formatting to comply with Ozzy's standards

--- src/WPILib.h
+++ src/WPILib.h
@@ -1,19 +1,34 @@
+/*********************************************************************************//**
+* @file WPILib.h
+* @author Jacob Hegna
+* Header file for mocking the robot API
+*************************************************************************************/
+
 #ifndef __WPILIB_H_
 #define __WPILIB_H_
 
-#define START_ROBOT_CLASS(a) 
-#define NULL 0
+/*************************************************************************************
+* Defines
+*************************************************************************************/
+#define 					START_ROBOT_CLASS(a) /**< Entry macro for WPILib        */
+#define 					NULL 0
 
 #ifdef _WIN32
-typedef int UInt32;
-typedef int UInt8;
+typedef int 				UInt32;				/**< VC++ isn't fully C99 compliant */
+typedef int 				UInt8;
 #endif
 
+/*********************************************************************************//**
+* Global functions
+*************************************************************************************/
 void Wait(float a);
 
-class ValueClass {
-public:
-	enum Value {
+/*********************************************************************************//**
+* Non-WPI class that represents all possible WPI enum values
+*************************************************************************************/
+class ValueClass {							/**< Normally this is defined in every  */
+public:										/**< WPI class, we are defining it once */
+	enum Value {							/**< and inheriting it     		        */
 		kReverse,
 		kForward,
 		kOn,
@@ -22,6 +37,9 @@ public:
 	};
 };
 
+/*********************************************************************************//**
+* Iterative Robot Class
+*************************************************************************************/
 class IterativeRobot {};
 
 class Compressor {
@@ -31,12 +49,18 @@ public:
 
 };
 
+/*********************************************************************************//**
+* RobotDrive class
+*************************************************************************************/
 class RobotDrive {
 public:
 	RobotDrive( int a, int b, int c, int d ) {}
 	void MecanumDrive_Cartesian( float a, float b, float c ) {}
 };
 
+/*********************************************************************************//**
+* Joystick class
+*************************************************************************************/
 class Joystick {
 public:
 	Joystick( int a ) {}
@@ -46,12 +70,18 @@ public:
     float GetZ() { return 0.f; }
 };
 
-class Talon : public ValueClass{
+/*********************************************************************************//**
+* Talon class
+*************************************************************************************/
+class Talon : public ValueClass {
 public:
 	Talon( int a ) {}
     void Set( float a, UInt8 b = 0 ) {}
 };
 
+/*********************************************************************************//**
+* DoubleSolenoid class
+*************************************************************************************/
 class DoubleSolenoid : public ValueClass {
 public:
 	DoubleSolenoid( int a, int b ) {}
--- src/fcntl.h
+++ src/fcntl.h
@@ -1,3 +1,9 @@
+/*********************************************************************************//**
+* @file fcntl.h
+* @author Jacob Hegna
+* Mock header file for testing the source code on non-frc compilers
+*************************************************************************************/
+
 #ifndef __FCNTL_H_
 #define __FCNTL_H_
 
